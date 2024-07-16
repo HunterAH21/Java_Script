226d10d462b43c73be4e23ce761d010f7bf5e3c6
HunterAH21 <116879213+HunterAH21@users.noreply.github.com>
2024-07-16 16:35:04 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//first program
 let arr = [1,2,3,4,5,6,7,8,9];
 let max = 0;
 let min = 10;
@@ -13,7 +15,7 @@ for(let i = 0;i<arr.length;i++){
 console.log("the largest and smallest number in the array ")
 console.log(max);
 console.log(min);
-
+//second program
 let arr1 = [1,2,3,4,5,6,7,8,9];
 arr1.push(10)
 console.log("push "+arr1)
@@ -23,7 +25,7 @@ arr1.unshift(0);
 console.log("unshift "+arr1)
 arr1.shift()
 console.log("shift "+arr1)
-
+//third program
 console.log("sorting of arrays");
 
 let arr2 = [7,8,9,6,5,4,3,2,1];
@@ -31,10 +33,11 @@ let arr2 = [7,8,9,6,5,4,3,2,1];
 let arr3 = arr2.sort();
 
 console.log(arr3);
-
+//fourth program
 console.log("merging of array");
 let arr4 = arr1 - arr2;
 console.log(arr4);
+//fifth program
 console.log("Searching");
 console.log(arr2.indexOf(1));
 
